4aaf035b6468c5f028c542f45ff7cf3516f1347a
peale <liping@zhangyue.com>
2017-08-23 21:12:37 +0800

    delete store

--- src/page/page.vue
+++ src/page/page.vue
@@ -1,10 +1,8 @@
 <script>
 	export default {
 		render: function(h){
-			var isRender = this.$store.state.isRender;
-			var tempArr = [];
-			console.log(this.$store.state)
-			this.$store.state.tree.forEach(function(item, idx, arr){
+			//tree post 过来的数据
+			tree.forEach(function(item, idx, arr){
 				var tempName = item.id;
 				var temp = require('@/components/widgets/'+tempName+'/'+tempName+'.vue')
 				tempArr.push(h(temp, {
